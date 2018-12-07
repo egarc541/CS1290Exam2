85d427b476919cfc369477beb590b851a2cf6e80
egarc541 <egarcia88@utep.edu>
2018-12-07 01:53:52 -0700

    Create FindDuplicateNumber.java

---
+++ FindDuplicateNumber/FindDuplicateNumber.java
@@ -0,0 +1,12 @@
+public class FindDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
