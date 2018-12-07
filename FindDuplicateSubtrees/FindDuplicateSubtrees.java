3dcd1e3dc42d18cfc49d69636a0320dfc6faff5d
egarc541 <egarcia88@utep.edu>
2018-12-07 04:57:12 -0700

    Update FindDuplicateSubtrees.java

--- FindDuplicateSubtrees/FindDuplicateSubtrees.java
+++ FindDuplicateSubtrees/FindDuplicateSubtrees.java
@@ -1,25 +1,19 @@
 public class FindDuplicateSubtrees {
-    int t;
-    Map<String, Integer> trees;
-    Map<Integer, Integer> count;
+    private int t = 0;
+    private Map<String, int[]> trees; // serialized subtree -> {index, count}
     List<TreeNode> ans;
-
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        t = 1;
-        trees = new HashMap();
-        count = new HashMap();
-        ans = new ArrayList();
-        lookup(root);
+        trees = new HashMap<>();
+        ans = new ArrayList<>();
+        getIndex(root);
         return ans;
     }
 
-    public int lookup(TreeNode node) {
-        if (node == null) return 0;
-        String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
-        int uid = trees.computeIfAbsent(serial, x-> t++);
-        count.put(uid, count.getOrDefault(uid, 0) + 1);
-        if (count.get(uid) == 2)
-            ans.add(node);
-        return uid;
+    private int getIndex(TreeNode root) {
+        if (root == null) return -1;
+        String serial = root.val + "," + getIndex(root.left) + "," + getIndex(root.right);
+        int[] arr = trees.computeIfAbsent(serial, x -> new int[]{t++, 0});
+        if (++arr[1] == 2) ans.add(root);
+        return arr[0];
     }
 }
